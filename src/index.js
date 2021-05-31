b090b62ad6288d01ec0a9295a5d442a812f0b1e4
Mika Andrianarijaona <mikaoelitiana@gmail.com>
2021-05-31 12:31:13 +0200

    fix: use single quote and remove semi

--- src/index.js
+++ src/index.js
@@ -1,41 +1,41 @@
-import t from "prop-types";
-import React from "react";
+import t from 'prop-types'
+import React from 'react'
 
 function makeLookup(arr, prop) {
-  let lkup = {};
+  let lkup = {}
   for (let i = 0, l = arr.length; i < l; i++) {
     if (prop) {
-      lkup[arr[i][prop]] = true;
+      lkup[arr[i][prop]] = true
     } else {
-      lkup[arr[i]] = true;
+      lkup[arr[i]] = true
     }
   }
-  return lkup;
+  return lkup
 }
 
 function getItemsByProp(arr, prop, values) {
-  let items = [];
-  let found = 0;
-  let valuesLookup = makeLookup(values);
+  let items = []
+  let found = 0
+  let valuesLookup = makeLookup(values)
   for (
     let i = 0, la = arr.length, lv = values.length;
     i < la && found < lv;
     i++
   ) {
     if (valuesLookup[arr[i][prop]]) {
-      items.push(arr[i]);
-      found++;
+      items.push(arr[i])
+      found++
     }
   }
-  return items;
+  return items
 }
 
 const DEFAULT_CLASS_NAMES = {
-  button: "FilteredMultiSelect__button",
-  buttonActive: "FilteredMultiSelect__button--active",
-  filter: "FilteredMultiSelect__filter",
-  select: "FilteredMultiSelect__select",
-};
+  button: 'FilteredMultiSelect__button',
+  buttonActive: 'FilteredMultiSelect__button--active',
+  filter: 'FilteredMultiSelect__filter',
+  select: 'FilteredMultiSelect__select',
+}
 
 class FilteredMultiSelect extends React.Component {
   static propTypes = {
@@ -53,26 +53,26 @@ class FilteredMultiSelect extends React.Component {
     size: t.number,
     textProp: t.string,
     valueProp: t.string,
-  };
+  }
 
   static defaultProps = {
-    buttonText: "Select",
-    className: "FilteredMultiSelect",
+    buttonText: 'Select',
+    className: 'FilteredMultiSelect',
     classNames: {},
-    defaultFilter: "",
+    defaultFilter: '',
     disabled: false,
-    placeholder: "type to filter",
+    placeholder: 'type to filter',
     selectedOptions: [],
     showFilter: true,
     size: 6,
-    textProp: "text",
-    valueProp: "value",
-  };
+    textProp: 'text',
+    valueProp: 'value',
+  }
 
   constructor(props) {
-    super(props);
+    super(props)
 
-    let { defaultFilter, selectedOptions } = props;
+    let { defaultFilter, selectedOptions } = props
     this.state = {
       // Filter text
       filter: defaultFilter,
@@ -80,7 +80,7 @@ class FilteredMultiSelect extends React.Component {
       filteredOptions: this._filterOptions(defaultFilter, selectedOptions),
       // Values of <options> currently selected in the <select>
       selectedValues: [],
-    };
+    }
   }
 
   componentDidUpdate(prevProps, prevState) {
@@ -99,95 +99,95 @@ class FilteredMultiSelect extends React.Component {
           ),
         },
         this._updateSelectedValues
-      );
+      )
     }
   }
 
   _getClassName(name, ...modifiers) {
-    let classNames = [this.props.classNames[name] || DEFAULT_CLASS_NAMES[name]];
+    let classNames = [this.props.classNames[name] || DEFAULT_CLASS_NAMES[name]]
     for (let i = 0, l = modifiers.length; i < l; i++) {
       if (modifiers[i]) {
         classNames.push(
           this.props.classNames[modifiers[i]] ||
             DEFAULT_CLASS_NAMES[modifiers[i]]
-        );
+        )
       }
     }
-    return classNames.join(" ");
+    return classNames.join(' ')
   }
 
   _filterOptions(filter, selectedOptions, options) {
-    if (typeof filter == "undefined") {
-      filter = this.state.filter;
+    if (typeof filter == 'undefined') {
+      filter = this.state.filter
     }
-    if (typeof selectedOptions == "undefined") {
-      selectedOptions = this.props.selectedOptions;
+    if (typeof selectedOptions == 'undefined') {
+      selectedOptions = this.props.selectedOptions
     }
-    if (typeof options == "undefined") {
-      options = this.props.options;
+    if (typeof options == 'undefined') {
+      options = this.props.options
     }
-    filter = filter.toUpperCase();
+    filter = filter.toUpperCase()
 
-    let { textProp, valueProp } = this.props;
-    let selectedValueLookup = makeLookup(selectedOptions, valueProp);
-    let filteredOptions = [];
+    let { textProp, valueProp } = this.props
+    let selectedValueLookup = makeLookup(selectedOptions, valueProp)
+    let filteredOptions = []
 
     for (let i = 0, l = options.length; i < l; i++) {
       if (
         !selectedValueLookup[options[i][valueProp]] &&
         (!filter || options[i][textProp].toUpperCase().indexOf(filter) !== -1)
       ) {
-        filteredOptions.push(options[i]);
+        filteredOptions.push(options[i])
       }
     }
 
-    return filteredOptions;
+    return filteredOptions
   }
 
   _selectRef = (select) => {
-    this._select = select;
-  };
+    this._select = select
+  }
 
   _onFilterChange = (e) => {
-    let filter = e.target.value;
+    let filter = e.target.value
     this.setState(
       {
         filter,
         filteredOptions: this._filterOptions(filter),
       },
       this._updateSelectedValues
-    );
-  };
+    )
+  }
 
   _onFilterKeyPress = (e) => {
-    if (e.key === "Enter") {
-      e.preventDefault();
+    if (e.key === 'Enter') {
+      e.preventDefault()
       if (this.state.filteredOptions.length === 1) {
-        let selectedOption = this.state.filteredOptions[0];
+        let selectedOption = this.state.filteredOptions[0]
         let selectedOptions = this.props.selectedOptions.concat([
           selectedOption,
-        ]);
-        this.setState({ filter: "", selectedValues: [] }, () => {
-          this.props.onChange(selectedOptions);
-        });
+        ])
+        this.setState({ filter: '', selectedValues: [] }, () => {
+          this.props.onChange(selectedOptions)
+        })
       }
     }
-  };
+  }
 
   _updateSelectedValues = (e) => {
-    let el = e ? e.target : this._select;
-    let selectedValues = [];
+    let el = e ? e.target : this._select
+    let selectedValues = []
     for (let i = 0, l = el.options.length; i < l; i++) {
       if (el.options[i].selected) {
-        selectedValues.push(el.options[i].value);
+        selectedValues.push(el.options[i].value)
       }
     }
     // Always update if we were handling an event, otherwise only update if
     // selectedValues has actually changed.
     if (e || String(this.state.selectedValues) !== String(selectedValues)) {
-      this.setState({ selectedValues });
+      this.setState({ selectedValues })
     }
-  };
+  }
 
   /**
    * Adds backing objects for the currently selected options to the selection
@@ -200,14 +200,14 @@ class FilteredMultiSelect extends React.Component {
         this.props.valueProp,
         this.state.selectedValues
       )
-    );
+    )
     this.setState({ selectedValues: [] }, () => {
-      this.props.onChange(selectedOptions);
-    });
-  };
+      this.props.onChange(selectedOptions)
+    })
+  }
 
   render() {
-    let { filter, filteredOptions, selectedValues } = this.state;
+    let { filter, filteredOptions, selectedValues } = this.state
     let {
       className,
       disabled,
@@ -216,14 +216,14 @@ class FilteredMultiSelect extends React.Component {
       size,
       textProp,
       valueProp,
-    } = this.props;
-    let hasSelectedOptions = selectedValues.length > 0;
+    } = this.props
+    let hasSelectedOptions = selectedValues.length > 0
     return (
       <div className={className}>
         {showFilter && (
           <input
             type="text"
-            className={this._getClassName("filter")}
+            className={this._getClassName('filter')}
             placeholder={placeholder}
             value={filter}
             onChange={this._onFilterChange}
@@ -234,7 +234,7 @@ class FilteredMultiSelect extends React.Component {
         <select
           multiple
           ref={this._selectRef}
-          className={this._getClassName("select")}
+          className={this._getClassName('select')}
           size={size}
           value={selectedValues}
           onChange={this._updateSelectedValues}
@@ -246,14 +246,14 @@ class FilteredMultiSelect extends React.Component {
               <option key={option[valueProp]} value={option[valueProp]}>
                 {option[textProp]}
               </option>
-            );
+            )
           })}
         </select>
         <button
           type="button"
           className={this._getClassName(
-            "button",
-            hasSelectedOptions && "buttonActive"
+            'button',
+            hasSelectedOptions && 'buttonActive'
           )}
           disabled={!hasSelectedOptions}
           onClick={this._addSelectedToSelection}
@@ -261,8 +261,8 @@ class FilteredMultiSelect extends React.Component {
           {this.props.buttonText}
         </button>
       </div>
-    );
+    )
   }
 }
 
-export default FilteredMultiSelect;
+export default FilteredMultiSelect
